46d01db89e92903450550cdf14cf332ce1414b01
RAPrograms <RAPrograms@outlook.com>
2025-12-16 23:55:22 +0000

    Writing formatted file working

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/xml"
 	"errors"
 	"fmt"
 	"net/http"
@@ -14,6 +16,31 @@ type icon_details struct {
 	Name         string
 	Contributors []string
 	SVG          string
+}
+
+func (details icon_details) format_svg() (string, error) {
+	decoder := xml.NewDecoder(strings.NewReader(details.SVG))
+	decoder.Strict = false
+
+	var buf bytes.Buffer
+	encoder := xml.NewEncoder(&buf)
+	encoder.Indent("", "  ")
+
+	for {
+		tok, err := decoder.Token()
+		if err != nil {
+			break
+		}
+		if err := encoder.EncodeToken(tok); err != nil {
+			return "", err
+		}
+	}
+
+	if err := encoder.Flush(); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
 
 func (details icon_details) format_contributors() string {
@@ -31,10 +58,16 @@ func (details icon_details) format_contributors() string {
 }
 
 func (details icon_details) to_string() string {
+	svg, svg_err := details.format_svg()
+	if svg_err != nil {
+		// TODO: add flag to force format
+		svg = details.SVG
+	}
+
 	return fmt.Sprintf("<!-- Icon sourced from Lucide.dev -->\n"+
 		"<!-- Name: %s -->\n"+
 		"<!-- Contributors: %s -->\n\n%s",
-		details.Name, details.format_contributors(), details.SVG,
+		details.Name, details.format_contributors(), svg,
 	)
 }
 
