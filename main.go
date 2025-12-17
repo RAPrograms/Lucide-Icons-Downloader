381fa6f4c05597bd28495c4e8c7464290894135f
RAPrograms <RAPrograms@outlook.com>
2025-12-17 07:39:31 +0000

    Adding log messages

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -30,7 +31,7 @@ func fetch_page(icon_name string) (*goquery.Document, error) {
 	return doc, nil
 }
 
-func extract_details(page *goquery.Document) icon_details {
+func extract_details(page *goquery.Document) (icon_details, error) {
 	// Get icon name
 	name := page.Find("main h1").Text()
 
@@ -54,37 +55,80 @@ func extract_details(page *goquery.Document) icon_details {
 	}
 
 	// Get svg code
-	svg, _ := goquery.OuterHtml(icon_target)
+	svg, reading_err := goquery.OuterHtml(icon_target)
+	if reading_err != nil {
+		return icon_details{}, reading_err
+	}
 
 	return icon_details{
 		Name:         name,
 		Contributors: contributors,
 		SVG:          svg,
-	}
+	}, nil
 }
 
-func save_icon(details icon_details) {
+func save_icon(details icon_details) error {
 	// Create a new file named "myfile.txt" in the same directory
 	file, err := os.Create("myfile.txt")
 	if err != nil { // Check for an error during file creation
-		panic(err)
+		return err
 	}
 	defer file.Close() // Ensure the file is closed when the function exits
 
 	_, write_err := file.WriteString(details.to_string())
 	if write_err != nil {
-		panic(write_err)
+		return err
 	}
+
+	return nil
 }
 
 func main() {
-	doc, err := fetch_page("move-right")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+
+	flag.Parse()
+
+	icon_names := flag.Args()
+	total_icons := len(icon_names)
+
+	fmt.Printf("\nProcessing %d icons...\n\n", total_icons)
+
+	state_output := func(err error) {
+		if err != nil {
+			fmt.Print("Failed")
+			fmt.Printf("\nError: %s \n'n", err)
+		} else {
+			fmt.Print("Done\n")
+		}
 	}
 
-	details := extract_details(doc)
+	for i, icon_name := range icon_names {
+		// Getting html document
+		fmt.Printf("[%d/%d] Fetching Webpage...    ", i+1, total_icons)
+		doc, fetching_err := fetch_page(icon_name)
 
-	save_icon(details)
+		state_output(fetching_err)
+		if fetching_err != nil {
+			continue
+		}
+
+		// Extracting details from html document
+		fmt.Printf("[%d/%d] Extracting details...  ", i+1, total_icons)
+		details, extreacting_err := extract_details(doc)
+
+		state_output(extreacting_err)
+		if extreacting_err != nil {
+			continue
+		}
+
+		// Save icon to file
+		fmt.Printf("[%d/%d] Extracting details...  ", i+1, total_icons)
+		saving_err := save_icon(details)
+
+		state_output(saving_err)
+		if saving_err != nil {
+			continue
+		}
+
+		fmt.Println("")
+	}
 }
